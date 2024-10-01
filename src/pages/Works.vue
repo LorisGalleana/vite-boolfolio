325b628cd0781736fe0daaefd6870ee39612a910
LorisGalleana <loris.galleana@gmail.com>
2024-10-01 17:40:27 +0200

    adding paginator

---
+++ src/pages/Works.vue
@@ -0,0 +1,72 @@
+<script>
+    import axios from 'axios';
+    import { store } from '../store/store';
+    import Loader from '../components/partials/Loader.vue';
+
+    export default{
+        name: 'Works',
+        components:{
+            Loader
+        },
+        data() {
+            return {
+                projects:[],
+                isLoading: false,
+                paginatorData:{
+                    current_page:1,
+                    links:[]
+                }
+            }
+        },
+        methods:{
+            getApi(){
+                axios.get(store.apiUrl + 'projects')
+                .then(response => {
+                    this.projects = response.data.data;
+                    this.paginatorData.current_page = result.data.current_page;
+                    this.paginatorData.links = result.data.links;
+                    this.isLoading = false
+                })
+                .catch(error => {
+                    console.log(error.message);
+                })
+            }
+        },
+        mounted(){
+            this.getApi()
+        }
+    }
+</script>
+
+
+<template>
+  <div>
+    <h1>I miei progetti</h1>
+    <div v-if="isLoading" class="loading"> 
+       <Loader /> 
+    </div>
+    <div v-else>
+        <ul>
+           <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+        </ul>
+        <div class="paginator">
+            <button v-for="(link, index) in paginatorData" :key="index">{{ link.label }}</button>   
+        </div> 
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.loading{
+    display: flex;
+    justify-content: center;
+    padding: 30px 0;
+}
+.paginator{
+    display: flex;
+    justify-content: center;
+    button{
+        margin: 0 3px;
+    }
+}
+</style>
