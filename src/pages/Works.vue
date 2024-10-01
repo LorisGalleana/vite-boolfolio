0668d8fb0d3fafab90eff9f2457512f9f89ab006
LorisGalleana <loris.galleana@gmail.com>
2024-10-01 18:40:48 +0200

    ESERCIZIO COMPLETATO

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -15,19 +15,43 @@
                 paginatorData:{
                     current_page:1,
                     links:[]
-                }
+                },
+                types:[],
+                technologies:[]
             }
         },
         methods:{
-            getApi(apiUrl){
+            getApi(apiUrl, type = 'projects'){
                 this.isLoading = true;
 
                 axios.get(apiUrl)
                 .then(response => {
-                    this.projects = response.data.data;
-                    this.paginatorData.current_page = response.data.current_page;
-                    this.paginatorData.links = response.data.links;
-                    this.isLoading = false
+                   
+                    if(type == 'projects'){
+                        this.isLoading = false
+                        this.projects = response.data.data;
+                        this.paginatorData.current_page = response.data.current_page;
+                        this.paginatorData.links = response.data.links;
+                    }
+                    else{
+                        this[type] = response.data;
+                    }
+                    
+                    /* switch (type) {
+                        case 'projects':
+                            this.isLoading = false
+                            this.projects = response.data.data;
+                            this.paginatorData.current_page = response.data.current_page;
+                            this.paginatorData.links = response.data.links;
+                            break;
+                        case 'types':
+                            this.types = response.data;
+                            break;
+                        case 'technologies':
+                            this.technologies = response.data;
+                            break;
+                    } */
+
                 })
                 .catch(error => {
                     console.log(error.message);
@@ -35,7 +59,9 @@
             }
         },
         mounted(){
-            this.getApi(store.apiUrl + 'projects')
+            this.getApi(store.apiUrl + 'projects', 'projects')
+            this.getApi(store.apiUrl + 'types', 'types')
+            this.getApi(store.apiUrl + 'technologies', 'technologies')
         }
     }
 </script>
@@ -47,13 +73,23 @@
     <div v-if="isLoading" class="loading"> 
        <Loader /> 
     </div>
-    <div v-else>
-        <ul>
-           <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
-        </ul>
-        <div class="paginator">
-            <button v-for="(link, index) in paginatorData.links" :key="index" v-html="link.label" :disabled="link.active || !link.url" @click="getApi(link.url)"></button>   
-        </div> 
+    <div class="content" v-else>
+        <div>
+            <ul>
+            <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+            </ul>
+            <div class="paginator">
+                <button v-for="(link, index) in paginatorData.links" :key="index" v-html="link.label" :disabled="link.active || !link.url" @click="getApi(link.url)"></button>   
+            </div> 
+        </div>
+        <div class="box-container">
+            <div class="box">
+                <button v-for="type in types" :key="type.id">{{ type.name }}</button>
+            </div>
+            <div class="box">
+                <button v-for="technology in technologies" :key="technology.id">{{ technology.name }}</button>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -69,6 +105,19 @@
     justify-content: center;
     button{
         margin: 0 3px;
+    }
+}
+.content{
+    display: flex;
+    .box{
+        border-radius: 5px;
+        border: 1px solid black;
+        padding: 15px;
+        margin: 15px;
+        max-width: 200px;
+        button{
+            margin: 3px;
+        }
     }
 }
 </style>
